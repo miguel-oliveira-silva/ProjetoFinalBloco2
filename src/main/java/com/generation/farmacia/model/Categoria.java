25b6a2242b10ed5cd04254880566102294413816
miguel-oliveira-silva <146590724+miguel-oliveira-silva@users.noreply.github.com>
2026-07-30 10:56:19 -0300

    Produto criado com relacionamento

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,10 +1,17 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -21,6 +28,10 @@ public class Categoria {
 	@Size(max = 255, message = "O atributo Descrição deve ter no máximo 255 caracteres")
 	@Column(length = 255)
 	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties(value = "categoria", allowSetters = true)
+	private List<Produto> produtos;
 	
 	public Long getId() {
 		return id;
---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,98 @@
+package com.generation.farmacia.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo nome é obrigatório")
+	@Size(min = 1, max = 255, message = "O atributo deve ter entre 1 e 255 caracteres.")
+	@Column(length = 255)
+	private String nome;
+	
+	@NotNull
+	@Column(precision = 10, scale = 2)
+	private BigDecimal preco;
+	
+	@NotBlank(message = "O atributo descrição é obrigatório!")
+	@Size(min = 5, max = 255, message = "O atributo deve ter entre 5 e 255 caracteres.")
+	@Column(length = 255)
+	private String descricao;
+
+	@NotBlank(message = "O atributo linkFoto é obrigatório")
+	@Size(min = 10, max = 255, message = "O atributo deve ter entre 10 e 255 caracteres.")
+	@Column(length = 255)
+	private String linkFoto;
+	
+	@ManyToOne
+	@JsonIgnoreProperties(value = "categoria", allowSetters = true)
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getLinkFoto() {
+		return linkFoto;
+	}
+
+	public void setLinkFoto(String linkFoto) {
+		this.linkFoto = linkFoto;
+	}
+	
+	
+}
